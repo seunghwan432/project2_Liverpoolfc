05ecd2ff2a9b82cd8c893f62de46c1b06a9cb6f3
seunghwan432 <seunghwan432@gmail.com>
2019-03-04 21:50:10 +0900

    1.2

--- js/src/news.js
+++ js/src/news.js
@@ -7,7 +7,7 @@
 
 
 
-
+// 뉴스 종류별로 이동
     news_bc.on('click',function(e){
         e.preventDefault();
         var news_order=news_bc.index(this);
@@ -19,15 +19,48 @@
         news_kind.eq(news_order).css({display:'block'});
         news_kind.eq(news_order).siblings().css({display:'none'});
 
+        $('.m_story').css({display:'none'});
+
         title.scrollTop(0);
+    });
+
+
+
+
+    // 뉴스 상세페이지
+
+    var main_news=$('.main_news');
+    var story=$('#newsBox').children('ul');
+
+
+    main_news.on('click',function(e){
+        e.preventDefault();
+        var main_news_parent=$(this).parent();
+        var main_news_ancestor=main_news_parent.parent();
+        console.log(main_news_ancestor.index());
+        var num=main_news_ancestor.index();
+
+
+        // news_kind.css({display:'none'});
+        story.eq(num).css({display:'block'});
+
+
+    
 
         
-       
+
+    });
+
+    $('.back_btn').on('click',function(e){
+        e.preventDefault();
+        story.eq(0).css({display:'none'});
+        story.eq(1).css({display:'none'});
+        story.eq(2).css({display:'none'});
+
+    });
 
 
-
-    })
-
+  
 
 
 
